ffad28be26d28300fdf0fc264aa1381393c9d785
mizllet <brian.tellez79@gmail.com>
2026-07-13 01:11:42 -0700

    Configuración manual de Cloudflare para Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
 import tailwindcss from '@tailwindcss/vite';
 
@@ -11,5 +12,6 @@ export default defineConfig({
   },
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  adapter: cloudflare()
 });
